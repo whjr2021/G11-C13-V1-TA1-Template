ddef41d1bb29431476f0cd0bcf22a10255556a1f
whjr2021 <91585088+whjr2021@users.noreply.github.com>
2021-11-23 10:46:43 +0530

    Add files via upload

---
+++ C13_TA1_Template.py
@@ -0,0 +1,8 @@
+# Define a function with 2 inputs - first_name, second_name. Name it as "full_name". 
+# Function must concatenate the two inputs seperated by a space.
+
+
+
+  
+# Call the "full_name" function by passing inputs and print the result function returns 
+
